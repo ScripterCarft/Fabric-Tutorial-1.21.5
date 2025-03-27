980ba027868cc79c0e9a96bb84183cb55914fd1e
BuildTools <unconfigured@null.spigotmc.org>
2025-03-27 17:00:45 +0100

    New Item

--- src/main/java/net/mineplug/tutorialmod/TutorialMod.java
+++ src/main/java/net/mineplug/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.mineplug.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mineplug.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
